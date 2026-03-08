436e839a4081d40336d26d82dbb6867f5a28afd1
Lino Marrero <linomarrero@DN0a1f0b54.SUNet>
2026-03-08 16:08:52 -0700

    Hero phone animation, waitlist API, deprecation fixes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -31,7 +31,7 @@ export default function Nav() {
           <img
             src="/calspaceslogofull.svg"
             alt="CalSpaces"
-            className="h-8 w-auto"
+            style={{ height: "32px", width: "auto" }}
           />
         </Link>
 
